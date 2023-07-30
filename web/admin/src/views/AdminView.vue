cfd1ee3e08a99433eb934b1023f87fa02a53495e
veryon-n <2630054911@qq.com>
2023-07-30 17:52:06 +0800

    [07/30:17:50] 配置vue前端开发环境

---
+++ web/admin/src/views/AdminView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    管理页面
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ web/admin/src/views/LoginView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    登录页面
+    <a-button type="primary">登录</a-button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
